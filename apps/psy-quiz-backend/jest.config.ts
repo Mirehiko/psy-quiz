eae596d3212247a8640c9c73c89ceef079a64152
mirehiko <v.ryazanov@reglab.ru>
2024-06-06 10:30:23 +0500

    init: frontend and backend apps

---
+++ apps/psy-quiz-backend/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'psy-quiz-backend',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/psy-quiz-backend',
+};
