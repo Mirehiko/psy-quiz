9d15148bbb652936ffa6a0cab35f677b739567ed
mirehiko <mirehiko@gmail.com>
2024-10-07 10:20:54 +0500

    build: server in docker & migrations updates

--- project/apps/psy-quiz-backend/src/migrations/1728289337459-config-migration.ts
+++ project/apps/psy-quiz-backend/src/migrations/1728289337459-config-migration.ts
@@ -2,7 +2,9 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 import { RoleEntity } from '../app/modules/common/role/schemas/role.entity';
 import { UserEntity } from '../app/modules/common/user/schemas/user.entity';
 
-export class Init1726050963725 implements MigrationInterface {
+export class ConfigMigration1728289337459 implements MigrationInterface {
+  name = 'ConfigMigration1728289337459';
+
   public async up(queryRunner: QueryRunner): Promise<void> {
     const roleRep = queryRunner.connection.getRepository(RoleEntity);
     const userRole = new RoleEntity();
